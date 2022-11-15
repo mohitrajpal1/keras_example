639a4e354ae5440c2d76a331565a9d551dbaec61
Mohit Rajpal <54977377+mohitrajpal1@users.noreply.github.com>
2022-11-15 14:24:53 +0800

    Create keras_network.py

---
+++ keras_network.py
@@ -0,0 +1,29 @@
+import keras
+from keras.datasets import cifar10, cifar100
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+             input_shape=(32, 32, 3)))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(self.num_classes))
+model.add(Activation('softmax'))
